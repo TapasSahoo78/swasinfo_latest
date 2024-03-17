9487bf6a1092db2db0c5280321dc0c5318e83593
TapasSahoo78 <tapasbca2015@gmail.com>
2024-03-17 16:54:54 +0000

    chnages

---
+++ storage/framework/views/3f2359ce11403d8cb6d6e7340f4a83484f6516e5.php
@@ -0,0 +1,6 @@
+<!-- <div class="login-footer">
+        <p>Copyrights 	&#169; 2023 Silvercoin. All Rights Reserved.</p>
+    
+      </div>
+  </div> -->
+<?php /**PATH /home/u932153640/domains/swasthfit.in/public_html/resources/views/admin/layouts/partials/_footer.blade.php ENDPATH**/ ?>
