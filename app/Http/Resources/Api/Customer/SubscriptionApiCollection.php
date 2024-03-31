308a5517d9933be5ec3205544f2f9d45af1570c4
TapasSahoo78 <tapasbca2015@gmail.com>
2024-03-31 18:20:32 +0530

    change api

--- app/Http/Resources/Api/Customer/SubscriptionApiCollection.php
+++ app/Http/Resources/Api/Customer/SubscriptionApiCollection.php
@@ -19,13 +19,14 @@ class SubscriptionApiCollection extends JsonResource
             'name' => $this->name,
             'id' => $this->id,
             'uuid' => $this->uuid,
-            'price'=>$this->price,
-            'frequently_purchased_title'=>$this->frequently_purchased_title,
-            'expiry_date'=>$this->expiry_date,
-            'day_validity'=>$this->day_validity,
-            'created_at'=>$this->created_at,
-            'updated_at'=>$this->updated_at,
-            'package'=>!empty($this->courses)? $this->courses:''
+            'price' => $this->price,
+            'frequently_purchased_title' => $this->frequently_purchased_title,
+            'expiry_date' => $this->expiry_date,
+            'day_validity' => $this->day_validity,
+            'is_ai_only' => $this->is_ai_only,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'package' => !empty($this->courses) ? $this->courses : ''
         ];
     }
 }
